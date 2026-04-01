5d4f0ccf5d98c0c8781271ce91cfc06b4b88abf7
Farix369 <farix369125@gmail.com>
2026-04-01 05:36:52 +0000

    Fix collector: move argparse to __main__ block

--- pipelines/stock_collector/collector.py
+++ pipelines/stock_collector/collector.py
@@ -1,12 +1,7 @@
 from api_client import history_price
 from transform import transform_data
 from db_writer import writer
-import argparse
-
-parser = argparse.ArgumentParser(description='Stock Data Pipeline: API data collection, data transformation, and writing to table')
-parser.add_argument('ticker')
-parser.add_argument('period')
-args = parser.parse_args()
+from cli import get_ticker_period
 
 def collect (ticker, period):
     print(f'Collecting {ticker} fro {period}')
@@ -16,4 +11,5 @@ def collect (ticker, period):
 
 
 if __name__ == '__main__':
-    collect(args.ticker, args.period)
+    ticker, period = get_ticker_period()
+    collect(ticker, period)
