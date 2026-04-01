83fc9c377a7e3e9b97398a109da667183237fa1d
Farix369 <farix369125@gmail.com>
2026-04-01 05:37:29 +0000

    Fix db_writer: atomic transaction with rollback and connection close

--- pipelines/stock_collector/db_writer.py
+++ pipelines/stock_collector/db_writer.py
@@ -2,17 +2,22 @@ from database.db_connection import connection_database
 
 def writer (df):
     connection = connection_database()
-    cursor =connection.cursor()
+    cursor = connection.cursor()
+    try:
+        for index, row in df.iterrows():
+            cursor.execute("""
+            INSERT INTO stock_prices (ticker, date, open, high, low, close, adj_close, volume)
+            VALUES (%s,%s,%s,%s,%s,%s,%s,%s)
+            ON CONFLICT DO NOTHING
+            """, (row['ticker'], row['date'], row['open'], row['high'], row['low'], row['close'], row['adj_close'], row['volume']))
 
-    for index, row in df.iterrows():
-        cursor.execute("""
-        INSERT INTO stock_prices (ticker, date, open, high, low, close, adj_close, volume)
-        VALUES (%s,%s,%s,%s,%s,%s,%s,%s)
-        ON CONFLICT DO NOTHING
-        """, (row['ticker'], row['date'], row['open'], row['high'], row['low'], row['close'], row['adj_close'], row['volume']))
-
-    connection.commit()     #transaction
-    cursor.close()
+        connection.commit()     #transaction
+    except Exception:
+        connection.rollback()
+        print("Connection Error. The modifications have been undone")
+    finally:
+        cursor.close()
+        connection.close()
 
 
 if __name__ == '__main__':
