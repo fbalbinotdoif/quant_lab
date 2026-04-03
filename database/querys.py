d423fea24b2c4b2f70a8a3a8f87c86b5861c366a
Farix369 <farix369125@gmail.com>
2026-04-03 06:23:00 +0000

    Add q_returns_indexed: returns df with date as index for benchmark alignment

--- database/querys.py
+++ database/querys.py
@@ -21,5 +21,27 @@ def q_returns(ticker,period):
     read = pd.read_sql((f"""SELECT adj_close FROM stock_prices WHERE
                         ticker = '{ticker}'  {date_filter}
                         """), connection)
-    
+    return read
+
+'QUERY RETURNS INDEXED-----------------------------------------------------------------------------------------------------------------------------------------------------'
+
+def q_returns_indexed(ticker,period):
+   
+    PERIOD_MAP = {
+    "1d": "1 day","5d": "5 days","1mo": "1 month","3mo": "3 month","6mo": "6 months","1y": "1 year","2y": "2 years","5y": "5 years","10y": "10 years"
+    }
+
+    if period == 'max':
+        date_filter = ""
+    elif period == 'ytd':
+        date_filter = "AND date >= DATE_TRUNC('year', CURRENT_DATE)"
+    else:
+        date_filter = f"AND date > (CURRENT_DATE - INTERVAL '{PERIOD_MAP[period]}')" 
+
+    connection = get_engine()
+
+    read = pd.read_sql(f"""SELECT date, adj_close FROM stock_prices 
+                    WHERE ticker = '{ticker}' {date_filter}
+                    ORDER BY date
+                    """, connection, index_col='date')
     return read
